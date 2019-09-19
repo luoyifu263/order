c6f071ab36583ab73162c5df87196b5fa40f8170
luoyifu2019 <2631022870@qq.com>
2019-09-19 14:29:07 +0800

    普通Result

---
+++ web-order/src/main/java/com/vip/weborder/utils/Result.java
@@ -0,0 +1,29 @@
+package com.vip.weborder.utils;
+
+import lombok.Data;
+
+/**
+
+ * @param <T>
+ */
+@Data
+public class Result<T> {
+    private int status;
+    private String msg;
+    private T data;
+
+    public static Result success(Object obj) {
+        Result result = new Result();
+        result.setData(obj);
+        result.setMsg("success");
+        result.setStatus(200);
+        return result;
+    }
+
+    public static Result error() {
+        Result result = new Result();
+        result.setMsg("error");
+        result.setStatus(404);
+        return result;
+    }
+}
