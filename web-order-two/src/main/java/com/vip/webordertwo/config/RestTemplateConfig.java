224bc37c8aac29dd372ef67589901b88137e0af8
TT <527962308@qq.com>
2019-09-20 20:30:13 +0800

    okhttp3

--- web-order-two/src/main/java/com/vip/webordertwo/config/RestTemplateConfig.java
+++ web-order-two/src/main/java/com/vip/webordertwo/config/RestTemplateConfig.java
@@ -1,22 +1,23 @@
-//package com.vip.webordertwo.config;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import okhttp3.OkHttpClient;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//@Configuration
-//@Slf4j
-//public class RestTemplateConfig {
-//    @Bean
-//    public OkHttpClient okHttpClient() {
-//        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-//        builder.connectTimeout(30, TimeUnit.SECONDS)
-//                .readTimeout(30, TimeUnit.SECONDS)
-//                .writeTimeout(30,TimeUnit.SECONDS)
-//                .retryOnConnectionFailure(true);
-//        return builder.build();
-//    }
-//}
+package com.vip.webordertwo.config;
+
+import lombok.Builder;
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@Builder
+public class RestTemplateConfig {
+    @Bean
+    public OkHttpClient okHttpClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30,TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true);
+        return builder.build();
+    }
+}
--- web-order-two/src/main/java/com/vip/webordertwo/controller/OrderController.java
+++ web-order-two/src/main/java/com/vip/webordertwo/controller/OrderController.java
@@ -1,14 +1,11 @@
 package com.vip.webordertwo.controller;
 
 import com.vip.webordertwo.domain.entity.OrderReturn;
-import com.vip.webordertwo.mapper.UserMapper;
 import com.vip.webordertwo.service.OrderService;
 import com.vip.webordertwo.utils.handler.ResultParameter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author Tang
